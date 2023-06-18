316bc994f9ace1d918ec20af9511932ad924d34e
Quantix <9021453+quantix-dev@users.noreply.github.com>
2023-06-18 04:07:25 +0100

    docs: setup vitepress for project documentation (#4)

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    lang: "en-GB",
+    title: "Verde",
+    description: "Syncs file systems between roblox studio and your editor of choice",
+    base: "verde",
+})
