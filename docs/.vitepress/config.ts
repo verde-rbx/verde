8b822b9e7bd111f658903171c9bd71ece6d9f3c3
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-06-18 04:20:51 +0100

    docs: correct docs basepath

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
     lang: "en-GB",
     title: "Verde",
     description: "Syncs file systems between roblox studio and your editor of choice",
-    base: "verde",
+    base: "/verde",
 })
