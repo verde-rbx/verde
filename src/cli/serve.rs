2b94a18ebcad378d2140323d0111f423b4d41af2
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-06 23:25:44 +0100

    feat(cli): add defaults for serve

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -3,26 +3,28 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
-use super::VerdeCommand;
 use crate::core::VerdeCore;
 use clap::{Parser, ValueHint};
 use std::{net::IpAddr, path::PathBuf};
 
+const DEFAULT_PROJECT: &str = "default.project.json";
+const DEFAULT_PORT: u32 = 3000;
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct ServeArgs {
-    #[arg(long, alias="path", value_hint=ValueHint::FilePath)]
+    #[arg(long, alias="path", value_hint=ValueHint::FilePath, default_value = DEFAULT_PROJECT)]
     project: PathBuf,
 
     #[arg(long, alias="address", value_hint=ValueHint::Hostname)]
-    host: IpAddr,
+    host: Option<IpAddr>,
 
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t = DEFAULT_PORT)]
     port: u32,
 }
 
-impl VerdeCommand for ServeArgs {
-    fn execute(self) {
+impl ServeArgs {
+    pub fn execute(self) {
         println!("Serving on port {}", self.port);
 
         let session = VerdeCore::new(self.project.as_path()).unwrap();
