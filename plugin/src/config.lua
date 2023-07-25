06e1c74b415f2d09e78ac10d81de2855e59b4a95
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-25 23:26:25 +0100

    feat: add config and state store

---
+++ plugin/src/config.lua
@@ -0,0 +1,27 @@
+--[[
+	Config
+
+	These values are put into the state store when the plugin starts,
+	This can be seen as a read-only version of the intiial plugin state.
+]]
+
+local DEFAULT_CONFIG = table.freeze {
+	PLUGIN_NAME = "Verde",
+	HOST = "localhost",
+	PORT = 34872,
+}
+
+local DEFAULT_SETTINGS = table.freeze {}
+
+local Config = {}
+
+function Config.getSetting<T>(_key: string): T
+	return DEFAULT_SETTINGS[_key]
+end
+
+-- Returns the config values
+function Config.getConfig()
+	return DEFAULT_CONFIG
+end
+
+return Config
---
+++ plugin/src/store.lua
@@ -0,0 +1,50 @@
+--[[
+    Fusion State Store
+
+	Used as a global state management system in the plugin.
+	Is essentially just a key value store of fusion states to be editing by any script
+]]
+local Config = require(script.Parent.config)
+local Fusion = require(script.Parent.packages.fusion)
+local Value = Fusion.Value
+
+local Store = {
+	_store = {},
+}
+
+-- Sets the specified value in the store
+function Store.set(_key: string, _value: unknown)
+	local state: Fusion.Value<typeof(_value)> | nil = Store.get(_key)
+	if state then
+		state:set(_value)
+	else
+		Store._store[_key] = Value(_value)
+	end
+end
+
+-- Gets the specified state in the store
+-- Will create an empty value if one is not found
+function Store.get<T>(_key: string): Fusion.Value<T> | nil
+	local state = Store._store[_key]
+	if not state then
+		state = Fusion.Value(nil)
+		Store._store[_key] = state
+	end
+	return state
+end
+
+-- Gets the specified value in the store
+function Store.getValue<T>(_key: string): T | nil
+	local state = Store.get(_key)
+	if state then
+		return state:get()
+	end
+	return nil
+end
+
+-- Add config values to store
+for k, v in Config.getConfig() do
+	Store.set(k, v)
+end
+
+return Store
