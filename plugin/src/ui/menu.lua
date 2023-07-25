3baba2e88e7e3b96ff54bed0765e94261239ddf6
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-25 23:26:16 +0100

    feat: add basic menu boilerplate

--- plugin/src/ui/menu.lua
+++ plugin/src/ui/menu.lua
@@ -1,9 +1,20 @@
 local Fusion = require(script.Parent.Parent.packages.fusion)
+local Store = require(script.Parent.Parent.store)
 
 local New = Fusion.New
+local Children = Fusion.Children
 
-return function()
-	return New("ScreenGui") {
-		Name = "Plugin",
+local Input = require(script.Parent.components.input)
+
+return function(_widget: DockWidgetPluginGui)
+	return New("Frame") {
+		Size = UDim2.fromScale(1, 1),
+		BackgroundTransparency = 1,
+		Visible = Store.get("UIShown"),
+		Parent = _widget,
+
+		[Children] = {
+			Input {},
+		},
 	}
 end
