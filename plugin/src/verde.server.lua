e6fb6575d093f812a19b8f7e441212a874576281
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-25 23:26:02 +0100

    feat: add plugin setup

---
+++ plugin/src/verde.server.lua
@@ -0,0 +1,29 @@
+--[[
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--]]
+
+if not plugin then
+	return error("Please run Verde in a plugin context")
+end
+
+local Store = require(script.Parent.store)
+
+-- Setup Plugin Widget
+local widgetSettings = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Right, true, false, 320, 210, 300, 210)
+local verdeWidget = plugin:CreateDockWidgetPluginGui("VerdeWidget", widgetSettings)
+verdeWidget.Title = "Verde"
+
+-- Setup plugin toolbar
+local verdeToolbar = plugin:CreateToolbar("Verde")
+local uiToggleBtn = verdeToolbar:CreateButton("VerdeUI", "Toggle Verde UI", "rbxassetid://1507949215")
+
+uiToggleBtn.Click:Connect(function()
+	local isShown = Store.getValue("UIShown") :: boolean
+	Store.set("UIShown", not isShown)
+	verdeWidget.Enabled = isShown
+end)
+
+-- Initialise UI
+require(script.Parent.ui.menu)(verdeWidget)
