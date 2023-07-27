65f593611bcdd70506aace1fafa8e293cf490e69
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-27 21:21:35 +0100

    chore: add betting typings to store

--- plugin/src/store.lua
+++ plugin/src/store.lua
@@ -3,18 +3,21 @@
 
 	Used as a global state management system in the plugin.
 	Is essentially just a key value store of fusion states to be editing by any script
-]]
+--]]
 local Config = require(script.Parent.config)
 local Fusion = require(script.Parent.packages.fusion)
+local Types = require(script.Parent.types)
 local Value = Fusion.Value
 
 local Store = {
-	_store = {},
+	_store = {} :: Types.Store,
 }
 
--- Sets the specified value in the store
+--[[
+	Sets the specified value in the store
+]]
 function Store.set(_key: string, _value: unknown)
-	local state: Fusion.Value<typeof(_value)> | nil = Store.get(_key)
+	local state: Fusion.Value<typeof(_value)?> = Store.get(_key)
 	if state then
 		state:set(_value)
 	else
@@ -22,19 +25,23 @@ function Store.set(_key: string, _value: unknown)
 	end
 end
 
--- Gets the specified state in the store
--- Will create an empty value if one is not found
-function Store.get<T>(_key: string): Fusion.Value<T> | nil
+--[[
+	Gets the specified state in the store
+	Will create an empty value if one is not found
+]]
+function Store.get<T>(_key: string): Fusion.Value<T?>
 	local state = Store._store[_key]
 	if not state then
-		state = Fusion.Value(nil)
+		state = Value(nil)
 		Store._store[_key] = state
 	end
 	return state
 end
 
--- Gets the specified value in the store
-function Store.getValue<T>(_key: string): T | nil
+--[[
+	Gets the specified value in the store
+]]
+function Store.getValue<T>(_key: string): T?
 	local state = Store.get(_key)
 	if state then
 		return state:get()
--- plugin/src/types.lua
+++ plugin/src/types.lua
@@ -1,8 +1,25 @@
 --[[
-    Verde Typings
+    UI Types
 --]]
+
+local Config = require(script.Parent.config)
 
 -- Component builder method
 export type Component<T> = (_props: T) -> Instance
+
+-- Panel builder method
+export type Panel = () -> Frame
+
+--[[ 
+    Store Types
+]]
+
+-- Store Menus
+export type Menus = "home" | "settings"
+
+export type Store = typeof(Config.getConfig()) & {
+	UIShown: boolean,
+	CurrentMenu: Menus,
+}
 
 return nil
