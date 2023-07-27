ce323e0657141e99e4d0cae62e9d12f2505e45db
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-07-27 23:09:15 +0100

    feat(plugin): improve store

--- plugin/src/store.lua
+++ plugin/src/store.lua
@@ -10,29 +10,27 @@ local Types = require(script.Parent.types)
 local Value = Fusion.Value
 
 local Store = {
-	_store = {} :: Types.Store,
+	_store = {
+		CurrentMenu = Value("home"),
+	} :: Types.Store,
 }
 
 --[[
 	Sets the specified value in the store
 ]]
 function Store.set(_key: string, _value: unknown)
-	local state: Fusion.Value<typeof(_value)?> = Store.get(_key)
-	if state then
-		state:set(_value)
-	else
-		Store._store[_key] = Value(_value)
-	end
+	local state: Fusion.Value<typeof(_value)?> = Store.get(_key, _value)
+	state:set(_value)
 end
 
 --[[
 	Gets the specified state in the store
 	Will create an empty value if one is not found
 ]]
-function Store.get<T>(_key: string): Fusion.Value<T?>
+function Store.get<T>(_key: string, _defaultValue: unknown?): Fusion.Value<T?>
 	local state = Store._store[_key]
 	if not state then
-		state = Value(nil)
+		state = Value(_defaultValue or "")
 		Store._store[_key] = state
 	end
 	return state
@@ -41,12 +39,8 @@ end
 --[[
 	Gets the specified value in the store
 ]]
-function Store.getValue<T>(_key: string): T?
-	local state = Store.get(_key)
-	if state then
-		return state:get()
-	end
-	return nil
+function Store.getValue<T>(_key: string, _defaultValue: unknown?): T?
+	return Store.get(_key, _defaultValue):get()
 end
 
 -- Add config values to store
