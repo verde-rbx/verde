49fd7295767a88714a53fced7090d7496a2cd097
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-08-02 19:07:30 +0100

    feat(plugin): create barebones session code

---
+++ plugin/src/session/init.session.lua
@@ -0,0 +1,29 @@
+--[[
+	Session
+
+	Manages the communuication between the Verde plugin and Verde cli.
+]]
+local HttpService = game:GetService("HttpService")
+
+export type SessionData = {}
+
+local Session = {} :: { _session: SessionData }
+
+--[[
+    Connects to a session.
+]]
+function Session.connect(_host: string?, _port: number?) end
+
+--[[
+    Disconnects from the current session.
+]]
+function Session.disconnect(_force: boolean?) end
+
+--[[
+    Returns the currently active session.
+]]
+function Session.getSession(): SessionData
+	return Session._session
+end
+
+return Session
