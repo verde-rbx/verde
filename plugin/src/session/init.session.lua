477ef6ab3e0a979f920487679cf3476fd9b0170a
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-08-06 18:05:12 +0100

    refactor(plugin): replace session with class

--- plugin/src/session/init.session.lua
+++ plugin/src/session/init.session.lua
@@ -7,23 +7,38 @@ local HttpService = game:GetService("HttpService")
 
 export type SessionData = {}
 
-local Session = {} :: { _session: SessionData? }
+local Session = {}
+Session.__index = Session
+
+function Session.new(_host: string?, _port: number?, _secure: boolean?)
+	local sessionData = {
+		BaseUrl = `{if _secure then "https" else "http"}://{_host}:{_port}`,
+	}
+
+	return setmetatable(sessionData, Session)
+end
+
+--[[
+    Makes a HTTP request to the session
+]]
+function Session:makeRequest(_path: string, _method: "GET" | "POST"?)
+	return HttpService:RequestAsync {
+		Url = `{self.BaseUrl}/{_path}`,
+		Method = _method or "GET",
+	}
+end
 
 --[[
     Connects to a session.
 ]]
-function Session.connect(_host: string?, _port: number?) end
+function Session:connect()
+	local connectionData = self:makeRequest("connect")
+	-- TODO: Process connection data (first request should include project setup)
+end
 
 --[[
     Disconnects from the current session.
 ]]
-function Session.disconnect(_force: boolean?) end
-
---[[
-    Returns the currently active session.
-]]
-function Session.getSession(): SessionData?
-	return Session._session
-end
+function Session:disconnect(_force: boolean?) end
 
 return Session
