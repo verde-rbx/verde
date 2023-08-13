e5927d2903865dc6e9187b1e870be6dbed3a95d7
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-08-13 05:08:16 +0100

    docs: add version info to docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,114 +1,127 @@
-import { DefaultTheme, defineConfig } from 'vitepress'
+import { DefaultTheme, defineConfig } from "vitepress";
+import pkg from "../package.json";
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
-    lang: "en-GB",
-    title: "Verde",
-    description: "Syncs file systems between roblox studio and your editor of choice",
+  lang: "en-GB",
+  title: "Verde",
+  description:
+    "Syncs file systems between roblox studio and your editor of choice",
 
-    lastUpdated: true,
-    cleanUrls: true,
+  lastUpdated: true,
+  cleanUrls: true,
 
-    sitemap: {
-        hostname: "https://verde.quantix.dev"
+  sitemap: {
+    hostname: "https://verde.quantix.dev",
+  },
+
+  head: [
+    ["link", { rel: "icon", href: "/verde.svg" }],
+    ["meta", { name: "theme-color", content: "#" }],
+    ["meta", { name: "og:type", content: "website" }],
+    ["meta", { name: "og:locale", content: "en" }],
+    ["meta", { name: "og:site_name", content: "Verde" }],
+  ],
+
+  markdown: {
+    theme: "monokai",
+    lineNumbers: true,
+  },
+
+  themeConfig: {
+    logo: "/verde.svg",
+    search: {
+      // TODO: Change to algolia once released
+      provider: "local",
     },
-
-    head: [
-        ['link', { rel: 'icon', href: '/verde.svg' }],
-        ['meta', { name: 'theme-color', content: '#' }],
-        ['meta', { name: 'og:type', content: 'website' }],
-        ['meta', { name: 'og:locale', content: 'en' }],
-        ['meta', { name: 'og:site_name', content: 'Verde' }]
+    nav: [
+      {
+        text: "Guide",
+        link: "/guide/introduction",
+        activeMatch: "/guide/",
+      },
+      {
+        text: "Reference",
+        link: "/reference/",
+        activeMatch: "/reference/",
+      },
+      {
+        text: pkg.version,
+        items: [
+          {
+            text: "Release Notes",
+            link: "https://github.com/quantix-dev/verde/releases/latest",
+          },
+          {
+            text: "Contributing",
+            link: "https://github.com/quantix-dev/verde/blob/main/.github/CONTRIBUTING.md",
+          },
+        ],
+      },
     ],
-
-    markdown: {
-        theme: 'monokai',
-        lineNumbers: true
+    socialLinks: [
+      { icon: "github", link: "https://github.com/quantix-dev/verde" },
+    ],
+    sidebar: {
+      "/guide/": {
+        base: "/guide/",
+        items: sidebarGuide(),
+      },
+      "/reference/": {
+        base: "/reference/",
+        items: sidebarReference(),
+      },
     },
-
-    themeConfig: {
-        logo: "/verde.svg",
-        search: {
-            // TODO: Change to algolia once released
-            provider: "local"
-        },
-        nav: [
-            {
-                text: 'Guide',
-                link: '/guide/introduction',
-                activeMatch: '/guide/'
-            },
-            {
-                text: 'Reference',
-                link: '/reference/',
-                activeMatch: '/reference/'
-            }
-        ],
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/quantix-dev/verde' }
-        ],
-        sidebar: {
-            '/guide/': {
-                base: '/guide/',
-                items: sidebarGuide()
-            },
-            '/reference/': {
-                base: '/reference/',
-                items: sidebarReference()
-            },
-        },
-        editLink: {
-            pattern: 'https://github.com/quantix-dev/verde/tree/main/docs/:path',
-            text: 'Contribute to this page on GitHub'
-        },
-        footer: {
-            message: 'Released under the Mozilla Public License Version 2.0.',
-        }
-    }
-})
+    editLink: {
+      pattern: "https://github.com/quantix-dev/verde/tree/main/docs/:path",
+      text: "Contribute to this page on GitHub",
+    },
+    footer: {
+      message: "Released under the Mozilla Public License Version 2.0.",
+    },
+  },
+});
 
 function sidebarGuide(): DefaultTheme.SidebarItem[] {
-    return [
-        {
-            text: 'Guide',
-            items: [
-                { text: "Introduction", link: 'introduction' },
-                { text: "Quick Start", link: 'quick-start' },
-            ]
-        },
-        {
-            text: 'Project',
-            base: 'project-',
-            items: [
-                { text: "Introduction", link: 'intro' },
-                { text: "Setup", link: 'setup' },
-            ]
-        },
-        {
-            text: 'Studio Plugin',
-            base: 'plugin-',
-            items: [
-                { text: "Introduction", link: 'intro' },
-                { text: "Setup", link: 'setup' }
-            ]
-        },
-    ]
+  return [
+    {
+      text: "Guide",
+      items: [
+        { text: "Introduction", link: "introduction" },
+        { text: "Quick Start", link: "quick-start" },
+      ],
+    },
+    {
+      text: "Project",
+      base: "project-",
+      items: [
+        { text: "Introduction", link: "intro" },
+        { text: "Setup", link: "setup" },
+      ],
+    },
+    {
+      text: "Studio Plugin",
+      base: "plugin-",
+      items: [
+        { text: "Introduction", link: "intro" },
+        { text: "Setup", link: "setup" },
+      ],
+    },
+  ];
 }
 
 function sidebarReference(): DefaultTheme.SidebarItem[] {
-    return [
+  return [
+    {
+      text: "Reference",
+      items: [
+        { text: "CLI", link: "cli" },
         {
-            text: 'Reference',
-            items: [
-                { text: 'CLI', link: 'cli' },
-                {
-                    text: "Project File", 
-                    base: 'project-file-',
-                    items: [
-                        { text: 'Overview', link: 'config' }
-                    ]
-                },
-            ]
+          text: "Project File",
+          base: "project-file-",
+          items: [{ text: "Overview", link: "config" }],
         },
-    ]
+      ],
+    },
+  ];
 }
