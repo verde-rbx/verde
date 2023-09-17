3a00e6b9834e80354ba661b78fd49d8304e85bd2
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-09-17 16:12:03 +0100

    feat(api): setup basic api routes

---
+++ src/api/mod.rs
@@ -0,0 +1,12 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+mod sync;
+use warp::Filter;
+
+/// Returns all api routes combined
+pub fn get_routes() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+    sync::sync()
+}
---
+++ src/api/sync.rs
@@ -0,0 +1,33 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+use warp::Filter;
+
+/// The main sync endpoint filter.
+pub fn sync() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+    filters::sync_handshake()
+}
+
+mod filters {
+    use super::handlers;
+    use warp::Filter;
+
+    /// POST /sync/handshake
+    /// Initiates a connection with the sync server.
+    pub fn sync_handshake() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::path!("sync" / "handshake").and_then(handlers::handshake)
+    }
+}
+
+mod handlers {
+    use std::convert::Infallible;
+    use warp::http::StatusCode;
+
+    struct SyncApi {}
+
+    pub async fn handshake() -> Result<impl warp::Reply, Infallible> {
+        Ok(StatusCode::OK)
+    }
+}
