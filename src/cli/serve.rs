21eaf00f5f0e14d02c7b4b6668bd403b9ce37b58
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-09-17 16:12:43 +0100

    chore: make license header consistent

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,8 +1,8 @@
 /**
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 use crate::core::{project, session, VerdeCore};
 use clap::{Parser, ValueHint};
 use std::{net::IpAddr, path::PathBuf};
