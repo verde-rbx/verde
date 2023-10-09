5512dddceb024f6e176633bd7b4dbdb0f8e9f1ec
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-10-09 22:33:58 +0100

    feat(adapter): setup barebones conversion adapters for projects

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -5,14 +5,23 @@
  */
 mod rojo;
 
-use std::path::Path;
+use std::{fs::File, path::Path};
+
+use crate::core::project::VerdeProject;
+
+enum Adapters {
+    Rojo,
+}
 
 /// Detects the project file type
-pub fn detect_project(path: &Path) -> Option<&Path> {
+fn detect_project(path: &Path) -> Option<Adapters> {
     if path.is_file() {
-        if path.ends_with(".project.json") {
-            return Some(path);
-        }
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        let suffix = file_name.split_once(".").unwrap().1;
+        return match suffix {
+            "project.json" => Some(Adapters::Rojo),
+            _ => None,
+        };
     } else {
         for _entry in path.read_dir().expect("Failed to read directory").flatten() {
             todo!("Check each entry for a project path");
@@ -23,4 +32,19 @@ pub fn detect_project(path: &Path) -> Option<&Path> {
 }
 
 /// Converts the project file type using an adapter to a Verde project
-pub fn convert_project() {}
+pub fn convert_project(path: &Path) -> Result<VerdeProject, ()> {
+    let project_type = detect_project(path);
+    let mut file = File::open(path).unwrap();
+
+    let converted = match project_type {
+        Some(Adapters::Rojo) => rojo::convert(&mut file),
+        None => todo!("Implement custom error struct for adapters"),
+    };
+
+    if let Ok(project) = converted {
+        return Ok(project);
+    }
+
+    // Utilise custom error struct
+    Err(())
+}
--- src/cli/convert.rs
+++ src/cli/convert.rs
@@ -6,12 +6,15 @@
 use clap::{Parser, ValueHint};
 use std::path::PathBuf;
 
+use crate::adapters::convert_project;
+
 #[derive(Parser)]
 /// Converts support project formats to the Verde project
 pub struct ConvertArgs {
+    // TODO: Allow to omit project path to auto detect project in CWD
     /// The project file to convert.
     #[arg(short, long, value_hint=ValueHint::FilePath)]
-    project: Option<PathBuf>,
+    project: PathBuf,
 
     /// The format to convert from.
     #[arg(short, long)]
@@ -19,5 +22,12 @@ pub struct ConvertArgs {
 }
 
 impl ConvertArgs {
-    pub fn execute(self) {}
+    pub fn execute(self) {
+        let path = self.project.as_path();
+        let project = convert_project(path);
+        if let Ok(project) = project {
+            project.save();
+            println!("Successfully converted project.")
+        }
+    }
 }
