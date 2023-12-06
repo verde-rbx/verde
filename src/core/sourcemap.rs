a3152fb81740fe2b88e045ce09620f74badbbed0
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2023-12-06 22:18:02 +0000

    feat(sourcemap): add sourcemap generation for defined project nodes

--- src/core/sourcemap.rs
+++ src/core/sourcemap.rs
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-use super::project::VerdeProject;
+use crate::core::project::{Node, VerdeProject};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 
@@ -26,17 +26,40 @@ pub struct VerdeSourcemap<'a> {
     pub children: Vec<VerdeSourcemap<'a>>,
 }
 
+fn populate_children(tree: &Node) -> Option<Vec<VerdeSourcemap<'_>>> {
+    if let Some(contents) = &tree.contents {
+        let mut children = Vec::with_capacity(contents.len());
+
+        for (key, child) in contents.iter() {
+            let child_contents = match child.contents {
+                Some(_) => populate_children(child),
+                None => Some(vec![]),
+            };
+
+            // TODO: Use node helper to get className
+            // TODO: Use node helper to get files under base path
+            children.push(VerdeSourcemap {
+                name: key,
+                class_name: key,
+                file_paths: vec![],
+                children: child_contents.unwrap(),
+            })
+        }
+
+        return Some(children);
+    }
+
+    None
+}
+
 impl<'a> VerdeSourcemap<'a> {
     pub fn from_project(project: &'a VerdeProject) -> Self {
-        // Get top level definition
-        // project.create_watcher();
-
+        let populated_children = populate_children(&project.tree);
         VerdeSourcemap {
             name: &project.name,
             class_name: "DataModel",
             file_paths: vec![],
-            children: vec![],
+            children: populated_children.unwrap(),
         }
     }
 }
-
