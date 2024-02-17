27b3ae637d19405d81bdb518b8ac7312721313a6
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2024-02-17 17:23:23 +0000

    style: update styling on sourcemap

--- src/core/sourcemap.rs
+++ src/core/sourcemap.rs
@@ -11,55 +11,56 @@ use std::path::Path;
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct VerdeSourcemap<'a> {
-    /// The name of the node.
-    pub name: &'a str,
+  /// The name of the node.
+  pub name: &'a str,
 
-    /// The Roblox class name.
-    pub class_name: &'a str,
+  /// The Roblox class name.
+  pub class_name: &'a str,
 
-    /// The file paths associated with the node.
-    #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub file_paths: Vec<&'a Path>,
+  /// The file paths associated with the node.
+  #[serde(skip_serializing_if = "Vec::is_empty")]
+  pub file_paths: Vec<&'a Path>,
 
-    /// Child nodes.
-    #[serde(skip_serializing_if = "Vec::is_empty")]
-    pub children: Vec<VerdeSourcemap<'a>>,
+  /// Child nodes.
+  #[serde(skip_serializing_if = "Vec::is_empty")]
+  pub children: Vec<VerdeSourcemap<'a>>,
 }
 
 fn populate_children(tree: &Node) -> Option<Vec<VerdeSourcemap<'_>>> {
-    if let Some(contents) = &tree.contents {
-        let mut children = Vec::with_capacity(contents.len());
+  if let Some(contents) = &tree.contents {
+    let mut children = Vec::with_capacity(contents.len());
 
-        for (key, child) in contents.iter() {
-            let child_contents = match child.contents {
-                Some(_) => populate_children(child),
-                None => Some(vec![]),
-            };
+    for (key, child) in contents.iter() {
+      // Get child elements
+      let child_contents = match child.contents {
+        Some(_) => populate_children(child),
+        None => Some(vec![]),
+      };
 
-            // TODO: Use node helper to get className
-            // TODO: Use node helper to get files under base path
-            children.push(VerdeSourcemap {
-                name: key,
-                class_name: key,
-                file_paths: vec![],
-                children: child_contents.unwrap(),
-            })
-        }
-
-        return Some(children);
+      // TODO: Use node helper to get className
+      // TODO: Use node helper to get files under base path
+      children.push(VerdeSourcemap {
+        name: key,
+        class_name: child.class_name.as_ref().unwrap_or(key),
+        file_paths: vec![],
+        children: child_contents.unwrap(),
+      })
     }
 
-    None
+    return Some(children);
+  }
+
+  None
 }
 
 impl<'a> VerdeSourcemap<'a> {
-    pub fn from_project(project: &'a VerdeProject) -> Self {
-        let populated_children = populate_children(&project.tree);
-        VerdeSourcemap {
-            name: &project.name,
-            class_name: "DataModel",
-            file_paths: vec![],
-            children: populated_children.unwrap(),
-        }
+  pub fn from_project(project: &'a VerdeProject) -> Self {
+    let populated_children = populate_children(&project.tree);
+    VerdeSourcemap {
+      name: &project.name,
+      class_name: "DataModel",
+      file_paths: vec![],
+      children: populated_children.unwrap(),
     }
+  }
 }
