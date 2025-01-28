3d6ce29f0f5d1bf42fee4edbb09ee70f33ec6457
quantix-dev <9021453+quantix-dev@users.noreply.github.com>
2025-01-28 22:06:37 +0000

    feat(transform): use generated node instance for payload

--- src/core/payload/transform.rs
+++ src/core/payload/transform.rs
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 use crate::core::payload::{PayloadAction, PayloadInstance};
 use crate::core::project::VerdeProject;
+use anyhow::bail;
 use notify::EventKind;
 use std::{fs, path::Path, sync::Arc};
 
@@ -27,14 +28,31 @@ pub fn transform_file(
 /// Transform a file into a Roblox script.
 fn transform_script(file_path: &Path, project: &Arc<VerdeProject>) -> anyhow::Result<PayloadInstance> {
   // Get file path
-  let instance_path = file_path.to_string_lossy().into_owned();
-  // TODO: Replace temporary instance path with calculation using project structure.
-  //       Chop off the 'project root' from the file_path then match to node path.
+  let root = project.root.as_ref().unwrap().canonicalize()?;
+  let mut stripped_path = file_path.strip_prefix(root)?;
+
+  // Find associated node
+  let Some(current_node) = project.find_node(stripped_path) else {
+    bail!("Unable to find associated node with path {:?}", stripped_path);
+  };
 
   // Get file contents
   let contents = fs::read_to_string(file_path)?;
+
+  // Create path from node
+  let Some(mut roblox_path) = current_node.roblox_path else {
+    bail!("Unable to find associated instance path for node.");
+  };
+
+  if let Some(node_path) = current_node.path {
+    stripped_path = stripped_path.strip_prefix(node_path)?;
+  }
+  if let Some(path) = stripped_path.to_str() {
+    roblox_path.push(path.to_string());
+  }
+
   Ok(PayloadInstance {
-    instance: instance_path,
+    instance: roblox_path,
     value: Some(contents),
   })
 }
